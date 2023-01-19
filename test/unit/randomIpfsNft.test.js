bd8c3677a82f7c91ace34b7ecfce574361730895
CouzDelCouzzz <quentin.diprima@gmail.com>
2023-01-19 17:10:43 +0100

    finalising th test

--- test/unit/randomIpfsNft.test.js
+++ test/unit/randomIpfsNft.test.js
@@ -1,5 +1,5 @@
 const { assert, expect } = require("chai")
-const { network, ethers, getNamedAccounts, deployments } = require("hardhat")
+const { network, ethers, deployments } = require("hardhat")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 
 !developmentChains.includes(network.name)
